0586085d775014ee54d432318535bf30f6ff4415
Julie Sparks <julie.sparks@cpcc.edu>
2010-10-25 13:10:06 -0400

    Moved views into views folder and updated urls.py accordingly

--- myschedule/urls.py
+++ myschedule/urls.py
@@ -2,26 +2,29 @@
 from django.conf.urls.defaults import include, patterns, url
 # from django.contrib import admin
 
-from myschedule import views, cart
+#from myschedule import views, cart
 
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^display_cart/((?:\w{3}-[\d\w]+-[\d\w]+-\w{2}-\d{4}/?)+)*$', cart.display_cart, name='display_cart'),
-    url(r'^delete_cartitem/(\w{3}-[\d\w]+-[\d\w]+-\w{2}-\d{4})/$', cart.delete_cartitem, name='delete_cartitem'),
-    url(r'^show_courses/', views.show_courses, name='show_courses'),
-    url(r'^show_sections/(\w{3})/([\d\w]+)/([\w]+)/$', views.show_sections, name='show_sections'),
-    url(r'^show_schedule/', cart.show_schedule, name='show_schedule'),
-    url(r'^delete_schedule/([\d]+)/$', cart.delete_schedule, name='delete_schedule'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^search/([\w ]*)/$', views.search, name='search'),
-    url(r'^cart/add/$', cart.add_item, name='add_item'),
-    url(r'^cart/get/$', cart.get_cart, name='get_cart'),
-    url(r'^cart/set/$', cart.set_cart, name='set_cart'),
-    url(r'^cart/save/$', cart.save_cart, name='save_cart'),
-    url(r'^schedule/email/$', cart.email_schedule, name='email_schedule'),
+urlpatterns = patterns('myschedule.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^search/$', 'search', name='search'),
+    url(r'^search/([\w ]*)/$', 'search', name='search'),
+    url(r'^show_courses/', 'show_courses', name='show_courses'),
+    url(r'^show_sections/(\w{3})/([\d\w]+)/([\w]+)/$', 'show_sections', name='show_sections'),
     # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('myschedule.views.cart',
+    url(r'^display_cart/((?:\w{3}-[\d\w]+-[\d\w]+-\w{2}-\d{4}/?)+)*$', 'display_cart', name='display_cart'),
+    url(r'^delete_cartitem/(\w{3}-[\d\w]+-[\d\w]+-\w{2}-\d{4})/$', 'delete_cartitem', name='delete_cartitem'),
+    url(r'^show_schedule/', 'show_schedule', name='show_schedule'),
+    url(r'^delete_schedule/([\d]+)/$', 'delete_schedule', name='delete_schedule'),
+    url(r'^cart/add/$', 'add_item', name='add_item'),
+    url(r'^cart/get/$', 'get_cart', name='get_cart'),
+    url(r'^cart/set/$', 'set_cart', name='set_cart'),
+    url(r'^cart/save/$', 'save_cart', name='save_cart'),
+    url(r'^schedule/email/$', 'email_schedule', name='email_schedule'),
 )
 
 # if settings.DEBUG:
