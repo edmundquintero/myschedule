c481752694bd4284e04c9d682237f1ffb7e2b662
Scott Sheffield <scott.sheffield@cpcc.edu>
2010-11-15 13:13:09 -0500

    Alpha of Search is working on Courses

--- myschedule/search_indexes.py
+++ myschedule/search_indexes.py
@@ -2,3 +2,15 @@ from haystack.indexes import *
 from haystack import site
 from myschedule import models
 
+class CourseIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    prefix = CharField(model_attr='prefix')
+    course_number = CharField(model_attr='prefix')
+    title = CharField(model_attr='title')
+    description = CharField(model_attr='description')
+    
+    def get_queryset(self):
+        return models.Course.objects.all()
+        
+
+site.register(models.Course, CourseIndex)
