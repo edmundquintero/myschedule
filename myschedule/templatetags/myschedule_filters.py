719b835d7153921338b9095d170076cea02b35fc
Julie Sparks <julie.sparks@cpcc.edu>
2011-02-04 10:18:37 -0500

    Added days_of_week custom filter

--- myschedule/templatetags/myschedule_filters.py
+++ myschedule/templatetags/myschedule_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def format_section(value):
     return value.replace('-', ' ').replace('fa', 'Fall').replace('sp', 'Spring')
+
+@register.filter(name='days_of_week')
+def days_of_week(value):
+    return value.upper().replace('R','Th').replace('S','Sa').replace('U','Su')
