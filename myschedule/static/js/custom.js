8000eecb7ce665b9ed5e64978e3e5e04113c8a0c
Julie Sparks <julie.sparks@cpcc.edu>
2011-02-25 16:12:28 -0500

    moved code from index.js into custom.js; deleted index.js; when search box gets focus, select text (so user does not have to highlight to change)

--- myschedule/static/js/custom.js
+++ myschedule/static/js/custom.js
@@ -40,6 +40,14 @@ $(function() {
         $('#view-full-schedule').hide();
     }
 
+    // Set a sample value for the query input field on when on the index page.
+    if ($('#id_query').val() == ''){
+        $('#id_query').val('ex. math, bus 121, mec');
+    }
+    $("#id_query").mouseup(function(e){
+        e.preventDefault();
+        });
+    $('#id_query').focus(function(){$(this).select();});
 });
 
 function createModalWindow(element, title, createCallback,
--- myschedule/static/js/index.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    // Set a sample value for the query input field on when on the index page.
-    if ($('#id_query').val() == ''){
-       $('#id_query').val('ex. math, bus 121, mec');
-    }
-});
