72b813c5e35a3ea86aca3fcecb733c70084f505e
Julie Sparks <julie.sparks@cpcc.edu>
2011-04-26 09:12:34 -0400

    Moved some settings from development.py to base.py; Always displays begin registration button, even during webadvisor downtime

--- myschedule/settings/base.py
+++ myschedule/settings/base.py
@@ -22,13 +22,48 @@ SESSION_COOKIE_NAME = 'myschedule_sessionid'
 
 THEME = '3g'
 
-DATA_CREDENTIALS = []
+DATABASES = {
+    'default': {
+        "ENGINE": "mysql",
+        "NAME": "myschedule",
+        "USER": "myschedule",
+        "PASSWORD": "myschedule",
+    }
+}
 
-CATALOG_URL = 'http://www.cpcc.edu/attending/catalog'
-
+HAYSTACK_SITECONF = 'myschedule.search_sites'
+HAYSTACK_SEARCH_ENGINE = 'solr'
+# Change haystack_solr_url before running update_index or rebuild_index
+HAYSTACK_SOLR_URL = ''
+HAYSTACK_INCLUDE_SPELLING = True
 # Heuristic Search Data
 HIGH_THRESHOLD = 1000
 MEDIUM_THRESHOLD = 300
 CRITERION_THRESHOLD = 10
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 10
 BLACKLIST = ['fuck', 'shit']
+
+CPAPI_URL_FORMAT = 'http://pas1-central.cpcc.edu/cpapi/?%s'
+CPAPI_KEY = ""
+
+# data_credentials contains username and password expected to be received in
+# header of bulk data push and seat count api
+DATA_CREDENTIALS = []
+
+CATALOG_URL = 'http://www.cpcc.edu/attending/catalog'
+
+# The following parameters are used to establish the connection to schedule2webadvisor
+# to submit the user's schedule to their preferred list in webadvisor.
+S2W_KEY_LOCATION = ''
+S2W_USER_AT_SERVER = 'snap2webadvisor@cis.cpcc.edu'
+S2W_KNOWNHOSTS = ''
+S2W_SEPARATOR = '}'
+S2W_RETURN_VALUES = ['Success', 'Empty Arguments' , 'already registered', 'valid student', 'Academic Level']
+S2W_DATATEL_URL = 'http://mycollege.cpcc.edu'
+# When finished testing, set S2W_TEST_SECTIONS to empty string
+S2W_TEST_SECTIONS = ''
+# s2w_unavailable_begin and end time should both be empty string if there
+# is no downtime for webadvisor (24 hour time format otherwise)
+S2W_UNAVAILABLE_BEGIN = '03:00:00'
+S2W_UNAVAILABLE_END = '07:30:00'
+DOWNTIME_MESSAGE = 'Class registration is unavailable between 3:00AM and 7:30AM.'
--- myschedule/settings/development.py
+++ myschedule/settings/development.py
@@ -2,15 +2,6 @@ from myschedule.settings.base import *
 
 DEBUG = TEMPLATE_DEBUG = True
 
-DATABASES = {
-    'default': {
-        "ENGINE": "mysql",
-        "NAME": "myschedule",
-        "USER": "myschedule",
-        "PASSWORD": "myschedule",
-    }
-}
-
 # In development, use the database for sessions, and disable caching.
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # Enabled caching - if you don't have memcached installed, switch to locmem.
@@ -23,30 +14,17 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # MEDIA_URL = APP_NAME + '/media/'
 
 CPAPI_URL_FORMAT = 'http://te409-05.cpcc.edu:5025/cpapi/?%s'
-#CPAPI_URL_FORMAT = 'http://pas1-central.cpcc.edu/cpapi/?%s'
 CPAPI_KEY = "myschedule_key"
 
 DATA_CREDENTIALS = ['testuser', 'testpass']
 
-HAYSTACK_SITECONF = 'myschedule.search_sites'
-HAYSTACK_SEARCH_ENGINE = 'solr'
 # Change me before running update_index or rebuild_index
 HAYSTACK_SOLR_URL = 'http://te409-05.cpcc.edu:8983/solr'
-HAYSTACK_INCLUDE_SPELLING = True
 
-# The following parameters are used to establish the connection to schedule2webadvisor
-# and submit the user's schedule to their preferred list in webadvisor.
 S2W_KEY_LOCATION = '/home/sje0388e/projects/django/myschedule/.ssh/privkey'
-S2W_USER_AT_SERVER = 'snap2webadvisor@cis.cpcc.edu'
 S2W_KNOWNHOSTS = '/home/sje0388e/projects/django/myschedule/.ssh/knownhosts'
-S2W_SEPARATOR = '}'
-S2W_RETURN_VALUES = ['Success', 'Empty Arguments' , 'already registered', 'valid student', 'Academic Level']
 #S2W_DATATEL_URL = 'https://watest-central.cpcc.edu/WADEV/WADEV'
 S2W_DATATEL_URL = 'http://mycollege.cpcc.edu/WATESTHR/WATESTHR'
 #S2W_DATATEL_URL = 'http://mycollege.cpcc.edu'
 # When finished testing, set S2W_TEST_SECTIONS to empty string
 S2W_TEST_SECTIONS = '70359}69664'
-# S2W_TEST_SECTIONS = ''
-S2W_UNAVAILABLE_BEGIN = '03:00:00'
-S2W_UNAVAILABLE_END = '07:30:00'
-DOWNTIME_MESSAGE = 'Class registration is unavailable between 3:00AM and 7:30AM.'
