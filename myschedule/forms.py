6a5653c824d8d4405e86aafe876bc814462d02d9
Julie Sparks <julie.sparks@cpcc.edu>
2011-10-11 12:35:13 -0400

    Refactored filtering for search form in forms.py

--- myschedule/forms.py
+++ myschedule/forms.py
@@ -28,108 +28,65 @@ class FilterSearchForm(SearchForm):
     end_date = forms.DateField(input_formats=date_formats, required=False)
 
     def search(self):
-        # TODO: Try refactoring some of this code!
         from datetime import datetime
         sqs = super(FilterSearchForm, self).search()
+
+        campus = ''
+        delivery_method = ''
+        start_date = None
+        end_date = None
+        filter_count = 0
+
+        if self.cleaned_data['campus']:
+            if self.cleaned_data['campus'] != 'all':
+                campus = self.cleaned_data['campus']
+                filter_count = filter_count + 1
+                sqs = sqs.filter(campuses__exact=campus)
+        if self.cleaned_data['delivery_method']:
+            if self.cleaned_data['delivery_method'] != 'all':
+                delivery_method = self.cleaned_data['delivery_method']
+                filter_count = filter_count + 1
+                sqs = sqs.filter(delivery_types__exact=delivery_method)
+        if self.cleaned_data['start_date']:
+            if self.cleaned_data['start_date'] != None:
+                start_date = self.cleaned_data['start_date']
+                filter_count = filter_count + 1
+                sqs = sqs.filter(start_dates__gte=start_date)
+        if self.cleaned_data['end_date']:
+            if self.cleaned_data['end_date'] != None:
+                end_date = self.cleaned_data['end_date']
+                filter_count = filter_count + 1
+                sqs = sqs.filter(end_dates__lte=end_date)
+
         # An important fact to keep in mind is that campuses, delivery types (or methods),
         # start dates and end dates are multi-value fields in the search index (because
         # this data comes from the sections and not the course).  That accounts for the
-        # additional filtering steps below.  In hindsight, the index probably should have
+        # additional filtering steps below.  (If more than one filter was specified, we
+        # must make sure to include the course only if it had section date that met all
+        # of the filtering criteria). In hindsight, the index probably should have
         # been at the section level (or potentially the meeting level). Maybe in version 3...
-        if self.cleaned_data['campus']:
-            if self.cleaned_data['campus'] != 'all':
-                sqs = sqs.filter(campuses__exact=self.cleaned_data['campus'])
-        if self.cleaned_data['delivery_method']:
-            if self.cleaned_data['delivery_method'] != 'all':
-                # Initial filtering of delivery method.
-                sqs = sqs.filter(delivery_types__exact=self.cleaned_data['delivery_method'])
-                # Despite specifying exact, if the delivery method is 'Online', it retains
-                # all delivery methods that contain the word 'Online', so will need to
-                # explicitly verify the courses really have sections with delivery method
-                # equal to 'Online'.
-                if self.cleaned_data['delivery_method'] == 'Online':
-                    for item in sqs:
-                        remove_item = True
-                        for delivery_type in item.delivery_types:
-                            if delivery_type == 'Online':
-                                remove_item = False
-                                break
-                        if remove_item == True:
-                            sqs = sqs.exclude(primary_key=item.pk)
-                # If the user wants to filter by both campus and delivery method, we have to
-                # go back through and make sure the delivery method that was retained applied
-                # to the campus that was requested.
-                if self.cleaned_data['campus'] != 'all':
-                    for item in sqs:
-                        i = 0
-                        remove_item = True
-                        if item.campuses:
-                            for campus in item.campuses:
-                                if campus == self.cleaned_data['campus'] and item.delivery_types[i] == self.cleaned_data['delivery_method']:
-                                    remove_item = False
-                                    break
-                                i = i + 1
-                        if remove_item == True:
-                            sqs = sqs.exclude(primary_key=item.pk)
-        # Unfortunately trying to filter dates through haystack and solr seems to be
-        # not functioning as expected (may be due to our versions of those apps - not
-        # sure at this point), so we'll just do it the manual way.
-        if self.cleaned_data['start_date'] or self.cleaned_data['end_date']:
+        if filter_count >= 2:
             for item in sqs:
-                if self.cleaned_data['start_date'] != None:
-                    remove_item = True
-                    if item.start_dates:
-                        i = 0
-                        for start_date in item.start_dates:
-                            temp_date = datetime.strptime(start_date.strip('T00:00:00Z'), '%Y-%m-%d').date()
-                            if temp_date >= self.cleaned_data['start_date']:
-                                if self.cleaned_data['end_date'] != None:
-                                    if item.end_dates:
-                                        temp_date = datetime.strptime(item.end_dates[i].strip('T00:00:00Z'), '%Y-%m-%d').date()
-                                        if temp_date <= self.cleaned_data['end_date']:
-                                            # If campus and/or delivery method filters were also specified,
-                                            # make sure this end date is for the specified campus and/or delivery method.
-                                            if (self.cleaned_data['campus'] == 'all' or 
-                                                   (self.cleaned_data['campus'] != 'all' and 
-                                                    item.campuses and
-                                                    item.campuses[i] == self.cleaned_data['campus'])):
-                                                if (self.cleaned_data['delivery_method'] == 'all' or 
-                                                       (self.cleaned_data['delivery_method'] != 'all' and 
-                                                        item.delivery_types and
-                                                        item.delivery_types[i] == self.cleaned_data['delivery_method'])):
-                                                    remove_item = False
-                                                    break    
-                                else:
-                                    if (self.cleaned_data['campus'] == 'all' or 
-                                           (self.cleaned_data['campus'] != 'all' and 
-                                            item.campuses and
-                                            item.campuses[i] == self.cleaned_data['campus'])):
-                                        if (self.cleaned_data['delivery_method'] == 'all' or 
-                                               (self.cleaned_data['delivery_method'] != 'all' and 
-                                                item.delivery_types and
-                                                item.delivery_types[i] == self.cleaned_data['delivery_method'])):
-                                            remove_item = False
-                                            break
-                            i = i + 1
-                else:
-                    if self.cleaned_data['end_date'] != None:
-                        remove_item = True
-                        if item.end_dates:
-                            i=0
-                            for end_date in item.end_dates:
-                                temp_date = datetime.strptime(end_date.strip('T00:00:00Z'), '%Y-%m-%d').date()
-                                if temp_date <= self.cleaned_data['end_date']:
-                                    if (self.cleaned_data['campus'] == 'all' or 
-                                           (self.cleaned_data['campus'] != 'all' and 
-                                            item.campuses and
-                                            item.campuses[i] == self.cleaned_data['campus'])):
-                                        if (self.cleaned_data['delivery_method'] == 'all' or 
-                                               (self.cleaned_data['delivery_method'] != 'all' and 
-                                                item.delivery_types and
-                                                item.delivery_types[i] == self.cleaned_data['delivery_method'])):
-                                            remove_item = False
-                                            break
-                                i = i + 1
+                i = 0
+                max_length = 0
+                remove_item = True
+                if item.campuses and campus != '':
+                    max_length = len(item.campuses)
+                if max_length == 0 and item.delivery_types and delivery_method != '':
+                    max_length = len(item.delivery_types)
+                if max_length == 0 and item.start_dates and start_date != None:
+                    max_length = len(item.start_dates)
+                if max_length == 0 and item.end_dates and end_date != None:
+                    max_length = len(item.end_dates)
+                while i <= max_length - 1:
+                    if ((campus == '' or (item.campuses and item.campuses[i] == campus)) and
+                        (delivery_method == '' or (item.delivery_types and item.delivery_types[i] == delivery_method)) and
+                        (start_date == None or (item.start_dates and 
+                                                datetime.strptime(item.start_dates[i].strip('T00:00:00Z'), '%Y-%m-%d').date() >= start_date)) and
+                        (end_date == None or (item.end_dates and datetime.strptime(item.end_dates[i].strip('T00:00:00Z'), '%Y-%m-%d').date() <= end_date))):
+                        remove_item = False
+                        break
+                    i = i + 1
                 if remove_item == True:
                     sqs = sqs.exclude(primary_key=item.pk)
         return sqs
--- myschedule/search_indexes.py
+++ myschedule/search_indexes.py
@@ -3,6 +3,18 @@ from haystack import site
 from myschedule import models
 from django.template import defaultfilters
 
+class DateMultiValueField(MultiValueField):
+    """
+       Supposedly this is subclassing a MultiValueField to create a
+       multivaluefield for dates.  The important thing to know here
+       is that if you use build_solr_schema, it will not set the
+       type to 'date' for these fields - you have to manually edit
+       schema.xml and change them to type 'date'.  This shouldn't
+       be a problem since, as noted below, we already had to manually
+       set other fields.
+    """
+    field_type = 'date'
+
 class CourseIndex(SearchIndex):
     """
 	Sort fields need to be defined as type string in solr schema.xml.
@@ -12,6 +24,9 @@ class CourseIndex(SearchIndex):
         title_sort from type text to type string.  If you don't, sorting
         by title will not work (index out of bounds error from solr)
         and sorting by course number will be incorrect.
+
+        delivery_type also has to be set to string in schema.xml (otherwise)
+        it mishandles the filtering for delivery type 'Online'.
     """
     text = CharField(document=True, use_template=True)
     primary_key = CharField(model_attr='pk')
@@ -23,8 +38,8 @@ class CourseIndex(SearchIndex):
     description = CharField(model_attr='description')
     campuses = MultiValueField()
     delivery_types = MultiValueField()
-    start_dates = MultiValueField()
-    end_dates = MultiValueField()
+    start_dates = DateMultiValueField()
+    end_dates = DateMultiValueField()
     popularity = CharField(model_attr='popularity')
     
     def prepare_campuses(self, obj):
