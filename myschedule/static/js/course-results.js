1d992e3150c880572fd8353ad2baec136e7356bf
Edmund Quintero <edmund.quintero@cpcc.edu>
2013-05-15 17:55:30 -0400

    Sections half bootstrapped

--- myschedule/static/js/course-results.js
+++ myschedule/static/js/course-results.js
@@ -1,8 +1,10 @@
 $(function(){
 
     $('.course .course-content').click(function(){
-        var link = $(this).attr('ref');
-        window.location = link;
+        if (!($(this).hasClass('disable-course'))){
+            var link = $(this).attr('ref');
+            window.location = link;
+        }
     });
 
 });
